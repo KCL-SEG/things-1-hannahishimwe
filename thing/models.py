d77ba951104f4dcc0522e453c0d97a0720b38b1c
Hannah Ishimwe <hannah.ishimwe@kcl.ac.uk>
2023-10-02 22:39:01 +0100

    Change model class name

--- thing/models.py
+++ thing/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-class ThingUser(models.Model):
+class Thing(models.Model):
     name = models.CharField(max_length=30
                             ,unique=True
                             ,blank=False
